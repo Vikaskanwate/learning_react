b9e42e4d6814724dc6653ffa24065bee83922483
Vikaskanwate <vikaskanwate32@gmail.com>
2025-02-12 01:00:04 +0530

    password

--- password_generator/src/App.jsx
+++ password_generator/src/App.jsx
@@ -1,5 +1,8 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./App.css"; 
 function App(){
+  const passwordRef = useRef(null);
+
   const [length,setLength] = useState(8);
   const [numberAllowed,setNumberAllowed] = useState(false);
   const [charAllowed,setCharAllowed] = useState(false);
@@ -11,13 +14,23 @@ function App(){
     if(numberAllowed) str += "123456789";
     if(charAllowed) str += "!@#$%^&*_";
 
-    for(let i = 1; i < Array.length;i++){
+    for(let i = 1; i < length;i++){
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
     setPassword(pass);
 
   },[length,numberAllowed,charAllowed,setPassword])
+
+  const copyPasswordToClipboard = useCallback(()=>{
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  }, [password]);
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numberAllowed,charAllowed,passwordGenerator]);
+
   return (
     <>
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-3 py-5 my-8 text-orange-500 bg-gray-400">
@@ -28,8 +41,10 @@ function App(){
         className="outline-none w-full py- px-3"
         placeholder="password"
         readOnly
+        ref={passwordRef}
         />
-        <button className="mx-2 bg-black p-2 rounded-lg" >Copy</button>
+        <button className="mx-2 bg-black p-2 rounded-lg"
+        onClick={copyPasswordToClipboard} >Copy</button>
       </div>
       <div className='flex text-sm gap-x-2'>
       <div className='flex items-center gap-x-1'>
