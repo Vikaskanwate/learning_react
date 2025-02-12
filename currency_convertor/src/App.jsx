35e8d654cc472cb71e4b9ad25e3f9eea7b5635c0
Vikaskanwate <vikaskanwate32@gmail.com>
2025-02-12 21:13:34 +0530

    react js

---
+++ currency_convertor/src/App.jsx
@@ -0,0 +1,11 @@
+import InputBox from "./components/InputBox";
+function App(){
+  return(
+    <>
+    <InputBox/>
+    <h1 className="text-3xl">CurrencyConvertor</h1>
+    </>
+  )
+}
+
+export default App;
