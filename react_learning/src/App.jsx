7bfc17c49f603fe4b7a02a923b7f756e789efad3
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-03 00:50:04 +0530

    practice

--- react_learning/src/App.jsx
+++ react_learning/src/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./components/Form"
 import Num from "./components/Num"
 import Card from './components/Card'
+import List from "./components/List"
 const  App = () => {
   return (
     <div>
       {/* <Form/> */}
-      <Card></Card>
+      {/* <Card></Card> */}
+      <List></List>
     </div>
   )
 } 
