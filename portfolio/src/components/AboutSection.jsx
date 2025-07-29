a4b07da097c3bbe1002acc9a7ecaf06bf1d0cf91
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-29 09:20:26 +0530

    Update AboutSection.jsx

--- portfolio/src/components/AboutSection.jsx
+++ portfolio/src/components/AboutSection.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Briefcase, Code, User } from 'lucide-react'
 const AboutSection = () => {
   return (
     <section id='about' className='py-20 px-4 relative'>
@@ -10,29 +10,62 @@ const AboutSection = () => {
         </h2>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-20 items-center mt-5">
-          <div className='space-y-6' > 
+          <div className='space-y-6' >
             <h3 className='text-2xl font-semibold'>Passionate Web Developer</h3>
             <p className='text-muted-foreground'>
               I specialize in creating responsive, accessible and performant web application using morden technologies.
             </p>
             <p className='text-muted-foreground'>
-              I'am Passionate about creating elegant solution to complex 
+              I'am Passionate about creating elegant solution to complex
               problem, and I'am constantly learning new technologies and techniques
               to stay at the forefront of the ever-evolving web landscape.
             </p>
 
             <div className='flex flex-col sm:flex-row gap-4 pt-4 justify-center'>
-                <a href="#contact" className='cosmic-button'>
-                  {""}
-                  Get In Touch
-                </a>
-                <a href="" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-color duration-300'>
+              <a href="#contact" className='cosmic-button'>
+                {""}
+                Get In Touch
+              </a>
+              <a href="" className='px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-color duration-300'>
                 Download Resume</a>
             </div>
           </div>
-            <div className=''>
-
+          <div className='grid grid-cols-1 gap-6'>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className='p-1 rounded-full bg-primary/10'>
+                  <Code className=" h-6 w-6 text-primary" />
+                </div>
+                <div className='text-left'>
+                  <h3 className='font-semibold text-lg'>Web Development</h3>
+                  <p className='text-muted-foreground'>Creating responsive websites and webapplication with morden frameworks.</p>
+                </div>
+              </div>
             </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className='p-1 rounded-full bg-primary/10'>
+                  <User className="h-6 w-6 text-primary" />
+                </div>
+                <div className='text-left'>
+                  <h3 className='font-semibold text-lg'>Backend Development</h3>
+                  <p className='text-muted-foreground'>Creating efficient backend application.</p>
+                </div>
+              </div>
+            </div>
+            <div className="gradient-border p-6 card-hover">
+              <div className="flex items-start gap-4">
+                <div className='p-1 rounded-full bg-primary/10'>
+                  <Briefcase className="h-6 w-6 text-primary" />
+                </div>
+                <div className='text-left'>
+                  <h3 className='font-semibold text-lg'>UI/UX Design</h3>
+                  <p className='text-muted-foreground'>Designing user interfaces and seamless user experiences.</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </section>
