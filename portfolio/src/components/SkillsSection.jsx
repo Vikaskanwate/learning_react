c4225af6c9a9b60079d813de39918b4a92ef49c3
Vikaskanwate <vikaskanwate32@gmail.com>
2025-07-31 21:20:56 +0530

    Update SkillsSection.jsx

--- portfolio/src/components/SkillsSection.jsx
+++ portfolio/src/components/SkillsSection.jsx
@@ -49,7 +49,7 @@ export const SkillsSection = ()=>{
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                     {filteredSkills.map((skill,key)=>(
                         <div key={key} className="bg-card p-6 rounded-lg shadow-xs card-hover">
-                            <div className="text-center mb-4">
+                            <div className="text-center mb-0">
                                 <h3 className="font-semibold text-lg">{skill.name}</h3>
                             </div>
                         </div>
