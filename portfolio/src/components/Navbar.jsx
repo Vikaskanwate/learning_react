87ff7fb92d2d89a24c1e39c9e759d64376a4dd80
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-03 10:34:49 +0530

    practice

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
         return () => window.removeEventListener("scroll", handleScroll)
     }, [])
     return (
-        <nav className={cn("fixedw-full z-40 transition-all duration-300",
+        <nav className={cn("fixed w-full z-40 transition-all duration-300",
             isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
         )}>
             <div className='container flex items-center justify-around'>
--- portfolio/src/components/ProjectSection.jsx
+++ portfolio/src/components/ProjectSection.jsx
@@ -1,4 +1,4 @@
-
+import { ArrowRight, ExternalLink, Github } from 'lucide-react'
 const projects = [
     {
         id: 1,
@@ -48,14 +48,28 @@ export const ProjectSection = () => {
                             </div>
 
                             <div className="p-6">
-                                <div className="flex flex-wrap gap-2 mb-4">
+                                <div className="flex flex-wrap gap-2 mb-3">
                                     {project.tags.map((tag) => (
                                         <span className="px-2 py-1 text-xs font-medium border text-primary/50 rounded-full bg-secondary text-secondary-foreground">{tag}</span>
                                     ))}
                                 </div>
+                                <h3 className="text-xl font-semibold mb-1 text-foreground">{project.title}</h3>
+                                <p className="text-muted-foreground text-sm mb-3">{project.description}</p>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex space-x-3">
+                                        <a href={project.githubUrl} target='_blank' className="text-foreground/80 hover:text-primary transition-colors duration-100">
+                                            <Github size={20} />
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     ))}
+                </div>
+                <div className='text-center mt-12'>
+                    <a href="" target='_blank' className='cosmic-button w-fit flex items-center mx-auto gap-2'>
+                        Check My Github <ArrowRight size={16}/>
+                    </a>
                 </div>
             </div>
         </section>
