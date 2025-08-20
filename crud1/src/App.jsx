678d38a9f7f113a68bd95ba8eb21a84e7cdeeb56
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-20 21:36:26 +0530

    practice

--- crud1/src/App.jsx
+++ crud1/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Posts from './components/Posts'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Posts/>
+    </div>
   )
 }
 
---
+++ crud1/src/api/PostApi.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostApi = () => {
+  return (
+    <div>PostApi</div>
+  )
+}
+
+export default PostApi
