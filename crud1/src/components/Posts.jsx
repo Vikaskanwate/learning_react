60eb223c75dec4fc6aa6700ccea6031dabf7c14c
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-20 22:02:11 +0530

    practice

--- crud1/src/components/Posts.jsx
+++ crud1/src/components/Posts.jsx
@@ -1,14 +1,30 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getPost } from '../api/PostApi';
 
 const Posts = () => {
   const [data,setData] = useState([]);
+
+  const getPostsData = async () =>{
+    const res = await getPost();
+    console.log(res);
+    setData(res.data);
+  }
+  useEffect(()=>{
+    getPostsData();
+  },[]);
   return (
     <div>
       <section className='section-post'>
         <ul>
           {
             data.map((curElem) =>{
-              
+              const {id,body,title} = curElem;
+              return <li key={id}>
+                <p>{title}</p>
+                <p>{body}</p>
+                <button>Add</button>
+                <button>Edit</button>
+              </li>
             })
           }
         </ul>
