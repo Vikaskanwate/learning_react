ab84424edc6661957935ee86b351a42b3647ba5e
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-29 07:00:50 +0530

    practice

--- crud2/src/components/Form.jsx
+++ crud2/src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { postData, updateData } from '../api/PostApi'
 const Form = ({ data, setData, updateDataApi, setUpdataDataApi }) => {
 
