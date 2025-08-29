bdc1a4979505b002e402c2ac3757582cf38a7260
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-29 06:20:32 +0530

    practice

--- crud2/src/components/Form.jsx
+++ crud2/src/components/Form.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Form = () => {
+const Form = ({data,setData}) => {
+ 
+  const [addData,setAddData] = useState({
+    title : "",
+    body :""
+  })
+
   return (
-    <div>Form</div>
+    <div style={{height:"70px",display:"flex",alignItems:"center",justifyContent:"center"}}>
+    <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="title"></label>
+          <input type="text" 
+          autoComplete='off'
+          name="title" 
+          id="title"
+          placeholder='Add Title' 
+          value={addData.title}
+          onChange={handleInputChange}/>
+        </div>
+        <div>
+          <label htmlFor="body"></label>
+          <input type="text" 
+          autoComplete='off'
+          name="body" 
+          id="body"
+          placeholder='Add body' 
+          value={addData.body}
+          onChange={handleInputChange}/>
+        </div>
+        <button type='submit'>Add</button>
+    </form>
+    </div>
   )
 }
 
