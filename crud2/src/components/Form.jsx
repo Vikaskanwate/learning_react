5bb5c59a5a85bb01bdc63cbdeb85729c6c85a633
Vikaskanwate <vikaskanwate32@gmail.com>
2025-08-29 07:00:18 +0530

    practice

--- crud2/src/components/Form.jsx
+++ crud2/src/components/Form.jsx
@@ -1,62 +1,93 @@
 import React, { useState } from 'react'
-import {postData} from '../api/PostApi'
-const Form = ({data,setData}) => {
- 
-  const [addData,setAddData] = useState({
-    title : "",
-    body :""
+import { postData, updateData } from '../api/PostApi'
+const Form = ({ data, setData, updateDataApi, setUpdataDataApi }) => {
+
+  const [addData, setAddData] = useState({
+    title: "",
+    body: ""
   })
 
-  const handleInputChange = (e) =>{
+  const handleInputChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
 
-    setAddData((prev =>{
+    setAddData((prev => {
       return {
         ...prev,
-        [name]:value,
+        [name]: value,
       }
     }))
   }
 
-  const addPostData = async ()=>{
+  const addPostData = async () => {
     const res = await postData(addData);
     console.log(res);
-    if(res.status === 201){
-      setData([...data,res.data])
-      setAddData({title:"",body:""});
+    if (res.status === 201) {
+      setData([...data, res.data])
+      setAddData({ title: "", body: "" });
     }
   }
 
-  const handleFormSubmit = (e) =>{
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     addPostData();
+    const action = e.nativeEvent.submitter.value;
+    if (action === "Add") {
+      addPostData();
+    } else if (action === "Edit") {
+      updatePostData();
+    }
   }
+
+  const updatePostData = async () => {
+    try {
+      const res = await updateData(updateDataApi.id, addData);
+      setData((prev) => {
+        return prev.map((curElem) => (
+          curElem.id === res.id ? res.data : curElem
+        ))
+      })
+      setAddData({ title: " ", body: " " });
+      setUpdataDataApi({});
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    updateDataApi &&
+      setAddData({
+        title: updateDataApi.title || "",
+        body: updateDataApi.body || ""
+      })
+  }, [updateDataApi]);
+
+  let isEmpty = Object.keys(updateDataApi).length === 0;
+
   return (
-    <div style={{height:"70px",display:"flex",alignItems:"center",justifyContent:"center"}}>
-    <form onSubmit={handleFormSubmit}>
+    <div style={{ height: "70px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <form onSubmit={handleFormSubmit}>
         <div>
           <label htmlFor="title"></label>
-          <input type="text" 
-          autoComplete='off'
-          name="title" 
-          id="title"
-          placeholder='Add Title' 
-          value={addData.title}
-          onChange={handleInputChange}/>
+          <input type="text"
+            autoComplete='off'
+            name="title"
+            id="title"
+            placeholder='Add Title'
+            value={addData.title}
+            onChange={handleInputChange} />
         </div>
         <div>
           <label htmlFor="body"></label>
-          <input type="text" 
-          autoComplete='off'
-          name="body" 
-          id="body"
-          placeholder='Add post' 
-          value={addData.body}
-          onChange={handleInputChange}/>
+          <input type="text"
+            autoComplete='off'
+            name="body"
+            id="body"
+            placeholder='Add post'
+            value={addData.body}
+            onChange={handleInputChange} />
         </div>
-        <button type='submit'>Add</button>
-    </form>
+        <button type="submit" value={isEmpty ? "Add" : "Edit"}>{isEmpty ? "Add" : "Edit"}</button>
+      </form>
     </div>
   )
 }
