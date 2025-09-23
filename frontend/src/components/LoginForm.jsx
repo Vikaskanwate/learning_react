5d316c9e2ca8558730012ddb9066b5afd17fc9eb
Vikaskanwate <vikaskanwate32@gmail.com>
2025-09-23 23:09:19 +0530

    practice

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function LoginForm() {
+function LoginForm({onLogin}) {
+    const [email,setEmail] = useState('');
+    const [password,setPassword] = useState('');
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        try{
+            const res = await api.post('/login',{email,password});
+            localStorage.setItem('token',res.data.token);
+            onLogin(res.data.user)
+        }catch(err){
+            alert('Login failed');
+        }
+    }
   return (
-    // <div>LoginForm</div>
-    <form >
+    <form onSubmit={handleSubmit}>
         <h2>Login</h2>
-        <input type="email" value={email} required/>
-        <input type="password" value={password} />
+        <input type="email" value={email} 
+        onChange={e => setEmail(e.target.value)}
+        placeholder='Email' 
+        required/>
+        <input type="password" value={password} 
+        onChange={e.setPassword(e.target.value)}
+        placeholder='password' 
+        required/>
         <button type='submit'>Login</button>
     </form>
   )
