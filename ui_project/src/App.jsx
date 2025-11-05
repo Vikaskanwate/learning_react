0cf17af08939005fe1b2042f08c218c7f14b7170
Vikaskanwate <vikaskanwate32@gmail.com>
2025-11-05 08:23:19 +0530

    practice

--- ui_project/src/App.jsx
+++ ui_project/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import Navbar from './component/section1/Navbar'
+import Section1 from './component/section1/section1'
+import Section2 from './component/section2/Section2'
 
 function App() {
   return (
-    <div><Navbar/></div>
+    <div>
+      <Section1/>
+      {/* <Section2/> */}
+    </div>
   )
 }
 
---
+++ ui_project/src/component/section1/Page1Content.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LeftContent from "./LeftContent";
+import RightContent from "./RightContent";
+const Page1Content = () => {
+    return (
+        <div className="py-10 flex gap-10 h-[90vh] bg-amber-50 px-18">
+            <LeftContent/>
+            <RightContent/>
+        </div>
+    )
+}
+
+export default Page1Content;
